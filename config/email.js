629c713f16b4f0a92c651402c8385b62079b611b
John Odey <johnodey335@gmail.com>
2026-03-20 05:20:24 -0700

    fix

--- config/email.js
+++ config/email.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: parseInt(process.env.EMAIL_PORT),
   secure: process.env.EMAIL_SECURE === 'true',
+  family: 4,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
